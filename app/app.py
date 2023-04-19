c77c792e4075a6bf968bfa164ab663f391320a29
climacs-net <41995177+climacs-net@users.noreply.github.com>
2023-04-19 22:06:06 +0800

    Create app.py
    create app.py with flask

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url = "https://api.thecatapi.com/v1/images/search"
+    response = requests.get(url)
+    cat_image = response.json()[0]["url"]
+    return render_template("index.html", cat_image=cat_image)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
